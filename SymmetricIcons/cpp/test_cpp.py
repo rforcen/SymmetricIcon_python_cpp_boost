fcf81d85322aa4072249b57ccf45f18498a53565
roberto forcen <robertoforcen@gmail.com>
2019-07-17 13:43:55 +0200

    Add files via upload

--- SymmetricIcons/cpp/test_cpp.py
+++ SymmetricIcons/cpp/test_cpp.py
@@ -1,28 +1,43 @@
 '''
     test of cpp symmetric icon
+    generate a grid with all presets
 '''
 
 from SymmetricIcon import SymmetricIcon
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+from math import sqrt
 
 mf = 1
-w, h, npreset, niters = mf * 1024, mf * 1024, 5, 900_000
+w, h, npreset, niters = mf * 1024, mf * 1024, 5, 1_000_000
 
-# symicon=SymmetricIcon(w, h, preset)
-# print(f'number of presets:{symicon.n_presets:}')
-# # symicon.set_parameters(2.48, -5.18, 29.99, -0.69, 0.095, 17, 1)
-# pixels = symicon.generate_image(iters) #[..., :3] for RGB conversion
+def timeIt():
+    print(f'generating preset:{npreset:}, {w:} x {h:} symmetric icon with {niters:,} iterations')
+    t = timer()
+    pixels = SymmetricIcon.gen(w, h, npreset, niters)
+    print(f'in {timer()-t:} secs')
 
-print(f'generating preset:{npreset:}, {w:} x {h:} symmetric icon with {niters:,} iterations')
-t = timer()
-pixels = SymmetricIcon.gen(w, h, npreset, niters)
-print(f'in {timer()-t:} secs')
+def plot_grid():
 
-fig = plt.figure(f'symmetric icons - {niters:,} iterations ')
-ax = plt.Axes(fig, [0., 0., 1., 1.])
-ax.set_axis_off()
-fig.add_axes(ax)
+    total_preset=36
+    nsq=int(sqrt(total_preset))
 
-plt.imshow(pixels)
-plt.show()
+    print(f'generating {total_preset:} symmetric icon presets in a {nsq:} x {nsq:} grid')
+
+    fig = plt.figure(f'symmetric icons - {niters:,} iterations ')
+    fig.subplots_adjust(bottom=0, top=1, left=0, right=1)
+
+    for preset in range(total_preset):
+        pixels = SymmetricIcon.gen(w, h, preset, int(niters)) # generate symmetric icon
+
+        plt.subplot(nsq, nsq, preset + 1)
+        plt.axis('off')
+        plt.Axes(fig, [0, 0, 1, 1])
+
+        plt.imshow(pixels)
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
+
+if __name__=='__main__':
+    plot_grid()
